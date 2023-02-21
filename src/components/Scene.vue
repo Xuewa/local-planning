54bafa66967922007e37156b8dce38ec878aa67f
xuewa <366653992@qq.com>
2023-02-21 23:25:21 +0800

    加载scene

---
+++ src/components/Scene.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref,onMounted } from 'vue'
+import WebScene from '@arcgis/core/WebScene'
+import SceneView from '@arcgis/core/views/SceneView'
+// import WebMap from "@arcgis/core/WebMap"
+
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+
+onMounted(() => {
+  const map = new WebScene({
+      portalItem: {
+        id: 'bceae470c9a04e5bb3ad42323c726c97',
+      },
+    });
+
+  const view = new SceneView({
+      map: map,
+      qualityProfile: 'medium',
+      container: 'scene'
+    })
+})
+</script>
+
+<template>
+  <div id="scene" ref="scene"></div>
+ 
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
